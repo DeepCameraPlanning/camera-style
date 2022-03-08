Fix data normalization

src/datamodules/datasets/flow_dataset.py
@@ -1,12 +1,12 @@
 import os
 import os.path as osp
-from typing import Any, Callable, Dict, List
+from typing import Any, Dict, List
 
 import numpy as np
 import torch
 from torch.utils.data import Dataset
 
-from src.utils.utils import load_pth
+from src.utils.file_utils import load_pth
 
 
 class TripletFlowDataset(Dataset):
@@ -16,8 +16,6 @@ class TripletFlowDataset(Dataset):
     :param unity_dir: path to the directory with precomputed Unity flows.
     :param prcpt_dir: path to the directory with precomputed flows.
     :param n_frames: number of flow frames in a sample.
-    :param unity_transforms: transformation to apply to unity flows.
-    :param prcpt_transforms: transformation to apply to precomputed flows.
     """
 
     def __init__(
@@ -26,8 +24,6 @@ class TripletFlowDataset(Dataset):
         unity_dir: str,
         prcpt_dir: str,
         n_frames: int,
-        unity_transforms: Callable,
-        prcpt_transforms: Callable,
     ):
         super().__init__()
 
@@ -36,8 +32,6 @@ class TripletFlowDataset(Dataset):
         self._prcpt_dir = prcpt_dir
 
         self._n_frames = n_frames
-        self._unity_transforms = unity_transforms
-        self._prcpt_transforms = prcpt_transforms
 
         self._clip_infos = self._get_clip_infos()
         self._sample_infos = self._get_sample_infos()
@@ -49,20 +43,10 @@ class TripletFlowDataset(Dataset):
             yield array[i : i + chunk_size]
 
     @staticmethod
-    def _load_flows(
-        flow_paths: List[str],
-        transforms: Callable,
-    ) -> torch.Tensor:
-        """
-        Load flows and and apply transformations.
-        Input shape: (T, W, H, C).
-        Output shape: (C, T, W, H).
-        """
-        flows = torch.stack(
-            [load_pth(flow_path) for flow_path in flow_paths]
-        ).permute([0, 3, 1, 2])
-        flows = transforms(flows) if transforms is not None else flows
-        flows = flows.permute([1, 0, 2, 3])
+    def _load_flows(flow_paths: List[str]) -> torch.Tensor:
+        """Load flows. Output shape: (C, T, W, H)."""
+        flows = torch.stack([load_pth(flow_path) for flow_path in flow_paths])
+        flows = flows.permute([3, 0, 1, 2])
 
         return flows
 
@@ -174,9 +158,9 @@ class TripletFlowDataset(Dataset):
         pos_paths = sample_info["positive_paths"]
         neg_paths = sample_info["negative_paths"]
 
-        anc_flows = self._load_flows(anc_paths, self._unity_transforms)
-        pos_flows = self._load_flows(pos_paths, self._prcpt_transforms)
-        neg_flows = self._load_flows(neg_paths, self._prcpt_transforms)
+        anc_flows = self._load_flows(anc_paths)
+        pos_flows = self._load_flows(pos_paths)
+        neg_flows = self._load_flows(neg_paths)
 
         sample_data = {
             "positive_clipname": sample_info["positive_clipname"],

src/datamodules/flow_datamodule.py
@@ -1,13 +1,10 @@
-from functools import partial
 import os.path as osp
 
 from pytorch_lightning import LightningDataModule
-import torch
 from torch.utils.data import DataLoader
-from torchvision.transforms import CenterCrop, Compose, Lambda  # Normalize
 
 from src.datamodules.datasets.flow_dataset import TripletFlowDataset
-from src.utils.utils import xy_to_polar, polar_to_xy, load_csv
+from src.utils.file_utils import load_csv
 
 
 class TripletFlowDataModule(LightningDataModule):
@@ -17,9 +14,6 @@ class TripletFlowDataModule(LightningDataModule):
     :param unity_dir: path to the directory with Unity flows.
     :param prcpt_dir: path to the directory with precomputed flows.
     :param n_frames: number of frames in a sample (fixed by the model).
-    :param frame_size: frame size to input (resizing)
-    :param unity_mod_max: maximum value of the unity module flow (norm).
-    :param prcpt_mod_max: maximum value of the precomputed module flow (norm).
     :param batch_size: size of batches.
     :param num_workers: number of workers.
     """
@@ -31,8 +25,6 @@ class TripletFlowDataModule(LightningDataModule):
         prcpt_dir: str,
         n_frames: int,
         frame_size: int,
-        unity_mod_max: float,
-        prcpt_mod_max: float,
         batch_size: int,
         num_workers: int,
     ):
@@ -43,43 +35,19 @@ class TripletFlowDataModule(LightningDataModule):
         val_split_path = osp.join(split_dir, "val.csv")
         self._val_clip_dirnames = load_csv(val_split_path)[0].tolist()
         test_split_path = osp.join(split_dir, "test.csv")
-        self._test_clip_dirnames = load_csv(test_split_path)[0].tolist()
+        self._test_clip_dirnames = load_csv(test_split_path)
+        self._test_clip_dirnames = (
+            self._test_clip_dirnames[0].tolist()
+            if self._test_clip_dirnames.size
+            else []
+        )
 
         self._unity_dir = unity_dir
         self._prcpt_dir = prcpt_dir
 
         self._n_frames = n_frames
-        self._frame_size = frame_size
-        self._unity_mod_max = unity_mod_max
-        self._prcpt_mod_max = prcpt_mod_max
-
         self.batch_size = batch_size
         self.num_workers = num_workers
-
-    @staticmethod
-    def _scale_module(
-        flows: torch.Tensor,
-        mod_max: float,
-    ) -> torch.Tensor:
-        """Scale module in polar coordinates between -1 adn 1."""
-        scaled_flows = torch.zeros_like(flows)
-        # scaled_flows[:, 0] = (2 * (flows[:, 0] - mod_max) / mod_max) + 1
-        scaled_flows[:, 0] = torch.ones_like(scaled_flows[:, 0])
-        scaled_flows[:, 1] = flows[:, 1]
-        return scaled_flows
-
-    def _transform(self, mod_max: float) -> Compose:
-        """Flow transformations to apply (T, C, H, W)."""
-        transform_list = [
-            CenterCrop(self._frame_size),
-            xy_to_polar,
-            Lambda(partial(self._scale_module, mod_max=mod_max)),
-            polar_to_xy,
-            # Normalize(mean=norm_mean, std=norm_std),
-        ]
-        transform = Compose(transform_list)
-
-        return transform
 
     def train_dataloader(self) -> DataLoader:
         """Load train set loader."""
@@ -88,8 +56,6 @@ class TripletFlowDataModule(LightningDataModule):
             unity_dir=self._unity_dir,
             prcpt_dir=self._prcpt_dir,
             n_frames=self._n_frames,
-            unity_transforms=self._transform(self._unity_mod_max),
-            prcpt_transforms=self._transform(self._prcpt_mod_max),
         )
 
         return DataLoader(
@@ -106,8 +72,6 @@ class TripletFlowDataModule(LightningDataModule):
             unity_dir=self._unity_dir,
             prcpt_dir=self._prcpt_dir,
             n_frames=self._n_frames,
-            unity_transforms=self._transform(self._unity_mod_max),
-            prcpt_transforms=self._transform(self._prcpt_mod_max),
         )
 
         return DataLoader(
@@ -123,8 +87,6 @@ class TripletFlowDataModule(LightningDataModule):
             unity_dir=self._unity_dir,
             prcpt_dir=self._prcpt_dir,
             n_frames=self._n_frames,
-            unity_transforms=self._transform(self._unity_mod_max),
-            prcpt_transforms=self._transform(self._prcpt_mod_max),
         )
 
         return DataLoader(

src/datamodules/preprocessing/get_stats.py
@@ -5,7 +5,8 @@ from typing import Tuple
 
 import torch
 
-from src.utils.utils import xy_to_polar
+from src.utils.flow_utils import FlowUtils
+from src.datamodules.preprocessing.flow_transforms import ResizeFlow
 
 Stats = Tuple[
     Tuple[float, float],
@@ -24,14 +25,21 @@ def parse_arguments() -> str:
         type=str,
         help="Path to the pth flow directory",
     )
+    parser.add_argument(
+        "--frame-size",
+        "-f",
+        type=float,
+        default=224,
+        help="Resizing size",
+    )
 
     args = parser.parse_args()
 
-    return args.pth_dir
+    return args.pth_dir, args.frame_size
 
 
 def print_stats(stats: Stats, coordinates: str):
-    """ """
+    """Display stats."""
     c1, c2 = ("x", "y") if coordinates == "xy" else ("r", "theta")
     mins, maxs, means, stds = stats
     print(f"min : {c1}={mins[0]:7.2f} - {c2}={mins[1]:7.2f} ")
@@ -41,7 +49,12 @@ def print_stats(stats: Stats, coordinates: str):
 
 
 def get_stats(xy_flows: torch.Tensor) -> Tuple[Stats]:
-    """ """
+    """Compute xy and polar min/max/mean/std."""
+    F = FlowUtils()
+    polar_flows = torch.stack(
+        [F.xy_to_polar(flow.permute([1, 2, 0])) for flow in xy_flows]
+    )
+
     # Get stats in Euclidean coordinates
     x_flows, y_flows = xy_flows[:, 0], xy_flows[:, 1]
     xy_flows_mins = (x_flows.min().item(), y_flows.min().item())
@@ -51,8 +64,7 @@ def get_stats(xy_flows: torch.Tensor) -> Tuple[Stats]:
     xy_stats = xy_flows_mins, xy_flows_maxs, xy_flows_means, xy_flows_stds
 
     # Get stats in polar coordinates
-    polar_flows = xy_to_polar(xy_flows)
-    m_flows, a_flows = polar_flows[:, 0], polar_flows[:, 1]
+    m_flows, a_flows = polar_flows[:, :, :, 0], polar_flows[:, :, :, 1]
     po_flows_mins = (m_flows.min().item(), a_flows.min().item())
     po_flows_maxs = (m_flows.max().item(), a_flows.max().item())
     po_flows_means = (m_flows.mean().item(), a_flows.mean().item())
@@ -64,13 +76,14 @@ def get_stats(xy_flows: torch.Tensor) -> Tuple[Stats]:
 
 if __name__ == "__main__":
     flows = []
-    pth_dir = parse_arguments()
+    pth_dir, frame_size = parse_arguments()
+    resizer = ResizeFlow((frame_size, frame_size))
     for flow_dirname in os.listdir(pth_dir):
         flow_dirpath = osp.join(pth_dir, flow_dirname)
         for flow_filename in sorted(os.listdir(flow_dirpath)):
             flow_path = osp.join(flow_dirpath, flow_filename)
-            flows.append(torch.load(flow_path))
-    flows = torch.stack(flows).permute([0, 3, 1, 2])
+            flows.append(resizer(torch.load(flow_path)))
+    flows = torch.stack(flows)
 
     xy_stats, po_stats = get_stats(flows)
     print("XY stats:")

src/models/modules/i3d.py
@@ -15,15 +15,10 @@ class MaxPool3dSamePadding(nn.MaxPool3d):
     def forward(self, x):
         # compute 'same' padding
         (batch, channel, t, h, w) = x.size()
-        # print t,h,w
-        # out_t = np.ceil(float(t) / float(self.stride[0]))
-        # out_h = np.ceil(float(h) / float(self.stride[1]))
-        # out_w = np.ceil(float(w) / float(self.stride[2]))
-        # print out_t, out_h, out_w
+
         pad_t = self.compute_pad(0, t)
         pad_h = self.compute_pad(1, h)
         pad_w = self.compute_pad(2, w)
-        # print pad_t, pad_h, pad_w
 
         pad_t_f = pad_t // 2
         pad_t_b = pad_t - pad_t_f
@@ -33,8 +28,6 @@ class MaxPool3dSamePadding(nn.MaxPool3d):
         pad_w_b = pad_w - pad_w_f
 
         pad = (pad_w_f, pad_w_b, pad_h_f, pad_h_b, pad_t_f, pad_t_b)
-        # print x.size()
-        # print pad
         x = F.pad(x, pad)
         return super(MaxPool3dSamePadding, self).forward(x)
 
@@ -88,15 +81,10 @@ class Unit3D(nn.Module):
     def forward(self, x):
         # compute 'same' padding
         (batch, channel, t, h, w) = x.size()
-        # print t,h,w
-        # out_t = np.ceil(float(t) / float(self._stride[0]))
-        # out_h = np.ceil(float(h) / float(self._stride[1]))
-        # out_w = np.ceil(float(w) / float(self._stride[2]))
-        # print out_t, out_h, out_w
+
         pad_t = self.compute_pad(0, t)
         pad_h = self.compute_pad(1, h)
         pad_w = self.compute_pad(2, w)
-        # print pad_t, pad_h, pad_w
 
         pad_t_f = pad_t // 2
         pad_t_b = pad_t - pad_t_f
@@ -106,10 +94,7 @@ class Unit3D(nn.Module):
         pad_w_b = pad_w - pad_w_f
 
         pad = (pad_w_f, pad_w_b, pad_h_f, pad_h_b, pad_t_f, pad_t_b)
-        # print x.size()
-        # print pad
         x = F.pad(x, pad)
-        # print x.size()
 
         x = self.conv3d(x)
         if self._use_batch_norm: