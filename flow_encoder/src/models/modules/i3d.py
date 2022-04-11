704e390cc5833b5140135b69f5bb2ea1ca280a52
robincourant <courant.robin@hotmail.fr>
2022-04-11 18:25:48 +0200

    Update autoencoder architexture
    - Reduce encoder
    - Increase and update decoder

--- flow_encoder/src/models/modules/i3d.py
+++ flow_encoder/src/models/modules/i3d.py
@@ -429,11 +429,224 @@ class InceptionI3d(nn.Module):
         return x
 
 
-class DecoderConv3D(nn.Module):
+class ReducedInceptionI3d(nn.Module):
+    """ """
+
+    VALID_ENDPOINTS = (
+        "Conv3d_1a_7x7",
+        "MaxPool3d_2a_3x3",
+        "Conv3d_2b_1x1",
+        "Conv3d_2c_3x3",
+        "MaxPool3d_3a_3x3",
+        "Mixed_3b",
+        "Mixed_3c",
+        "MaxPool3d_4a_3x3",
+        "Mixed_4b",
+        "Mixed_4c",
+        "Mixed_4d",
+        "Mixed_4e",
+        "Mixed_4f",
+        "MaxPool3d_5a_2x2",
+        "Mixed_5b",
+        "Mixed_5c",
+    )
+
+    def __init__(
+        self,
+        final_endpoint="Mixed_5c",
+        name="inception_i3d",
+        in_channels=2,
+    ):
+        if final_endpoint not in self.VALID_ENDPOINTS:
+            raise ValueError("Unknown final endpoint %s" % final_endpoint)
+
+        super(ReducedInceptionI3d, self).__init__()
+        self._final_endpoint = final_endpoint
+
+        if self._final_endpoint not in self.VALID_ENDPOINTS:
+            raise ValueError(
+                "Unknown final endpoint %s" % self._final_endpoint
+            )
+
+        self.end_points = {}
+        end_point = "Conv3d_1a_7x7"
+        self.end_points[end_point] = Unit3D(
+            in_channels=in_channels,
+            # output_channels=4,
+            output_channels=3,
+            kernel_shape=[7, 7, 7],
+            stride=(2, 4, 4),
+            padding=(3, 3, 3),
+            name=name + end_point,
+        )
+        if self._final_endpoint == end_point:
+            self.build()
+            return
+
+        end_point = "MaxPool3d_2a_3x3"
+        self.end_points[end_point] = MaxPool3dSamePadding(
+            kernel_size=[1, 3, 3], stride=(1, 2, 2), padding=0
+        )
+        if self._final_endpoint == end_point:
+            self.build()
+            return
+
+        end_point = "Conv3d_2b_1x1"
+        self.end_points[end_point] = Unit3D(
+            # in_channels=4,
+            # output_channels=4,
+            in_channels=3,
+            output_channels=3,
+            kernel_shape=[1, 1, 1],
+            padding=0,
+            name=name + end_point,
+        )
+        if self._final_endpoint == end_point:
+            self.build()
+            return
+
+        end_point = "Conv3d_2c_3x3"
+        self.end_points[end_point] = Unit3D(
+            # in_channels=4,
+            # output_channels=8,
+            in_channels=3,
+            output_channels=4,
+            kernel_shape=[3, 3, 3],
+            padding=1,
+            name=name + end_point,
+        )
+        if self._final_endpoint == end_point:
+            self.build()
+            return
+
+        end_point = "MaxPool3d_3a_3x3"
+        self.end_points[end_point] = MaxPool3dSamePadding(
+            kernel_size=[1, 3, 3], stride=(1, 2, 2), padding=0
+        )
+        if self._final_endpoint == end_point:
+            self.build()
+            return
+
+        end_point = "Mixed_3b"
+        self.end_points[end_point] = InceptionModule(
+            4, [2, 3, 4, 1, 1, 1], name + end_point
+        )
+        # 8, [4, 6, 8, 1, 2, 2], name + end_point
+        if self._final_endpoint == end_point:
+            self.build()
+            return
+
+        end_point = "Mixed_3c"
+        self.end_points[end_point] = InceptionModule(
+            8, [4, 4, 6, 2, 4, 2], name + end_point
+        )
+        # 16, [8, 8, 12, 2, 8, 4], name + end_point
+        if self._final_endpoint == end_point:
+            self.build()
+            return
+
+        end_point = "MaxPool3d_4a_3x3"
+        self.end_points[end_point] = MaxPool3dSamePadding(
+            kernel_size=[3, 3, 3], stride=(2, 2, 2), padding=0
+        )
+        if self._final_endpoint == end_point:
+            self.build()
+            return
+
+        end_point = "Mixed_4b"
+        self.end_points[end_point] = InceptionModule(
+            16, [6, 3, 10, 1, 2, 4], name + end_point
+        )
+        if self._final_endpoint == end_point:
+            self.build()
+            return
+
+        end_point = "Mixed_4c"
+        self.end_points[end_point] = InceptionModule(
+            22, [4, 4, 10, 1, 4, 4], name + end_point
+        )
+        # 48, [18, 8, 20, 1, 4, 6], name + end_point
+        if self._final_endpoint == end_point:
+            self.build()
+            return
+
+        end_point = "Mixed_4d"
+        self.end_points[end_point] = InceptionModule(
+            22, [2, 2, 12, 4, 4, 4], name + end_point
+        )
+        # 48, [12, 12, 26, 4, 6, 6], name + end_point
+        if self._final_endpoint == end_point:
+            self.build()
+            return
+
+        end_point = "Mixed_4e"
+        self.end_points[end_point] = InceptionModule(
+            22, [2, 2, 14, 6, 4, 4], name + end_point
+        )
+        # 50, [12, 14, 30, 6, 8, 8], name + end_point
+        if self._final_endpoint == end_point:
+            self.build()
+            return
+
+        end_point = "Mixed_4f"
+        self.end_points[end_point] = InceptionModule(
+            24, [4, 2, 16, 6, 4, 4], name + end_point
+        )
+        # 58, [12, 14, 30, 6, 8, 8], name + end_point
+        if self._final_endpoint == end_point:
+            self.build()
+            return
+
+        end_point = "MaxPool3d_5a_2x2"
+        self.end_points[end_point] = MaxPool3dSamePadding(
+            kernel_size=[2, 2, 2], stride=(2, 2, 2), padding=0
+        )
+        if self._final_endpoint == end_point:
+            self.build()
+            return
+
+        end_point = "Mixed_5b"
+        self.end_points[end_point] = InceptionModule(
+            28, [4, 2, 16, 6, 4, 4], name + end_point
+        )
+        # 58, [12, 14, 30, 6, 8, 8], name + end_point
+        if self._final_endpoint == end_point:
+            self.build()
+            return
+
+        end_point = "Mixed_5c"
+        self.end_points[end_point] = InceptionModule(
+            28, [6, 4, 18, 6, 4, 4], name + end_point
+        )
+        # 58, [16, 14, 32, 6, 8, 8], name + end_point
+        if self._final_endpoint == end_point:
+            self.build()
+            return
+
+    def build(self):
+        for k in self.end_points.keys():
+            self.add_module(k, self.end_points[k])
+
+    def forward(self, x):
+        for end_point in self.VALID_ENDPOINTS:
+            if end_point in self.end_points:
+                x = self._modules[end_point](x)
+
+        return x
+
+    def extract_features(self, x, avg_out=True):
+        for end_point in self.VALID_ENDPOINTS:
+            if end_point in self.end_points:
+                x = self._modules[end_point](x)
+
+        return x
+
+
+class SimpleDecoderConv3D(nn.Module):
     """3D CNN flow decoder."""
 
     def __init__(self, in_channels: int, latent_dim: int):
-        super(DecoderConv3D, self).__init__()
+        super(SimpleDecoderConv3D, self).__init__()
 
         ndf = latent_dim // 8
         # dconv0 out: 512, 4, 14, 14
@@ -457,7 +670,7 @@ class DecoderConv3D(nn.Module):
         # dconv3 out: 2, 16, 224, 224
         self.deconv3 = nn.Sequential(
             nn.ConvTranspose3d(
-                ndf, in_channels, (3, 6, 6), (1, 4, 4), 1, bias=False
+                ndf, in_channels, (3, 9, 9), (1, 7, 7), 1, bias=False
             ),
             # nn.Sigmoid(),
             nn.Tanh(),
@@ -471,25 +684,228 @@ class DecoderConv3D(nn.Module):
         return x
 
 
+class DecoderConv3D(nn.Module):
+    """3D CNN flow decoder."""
+
+    VALID_ENDPOINTS = (
+        "Conv3d_1_1x1",
+        "Upconv3d_2a",
+        "Conv3d_2b_3x3",
+        "Conv3d_2c_3x3",
+        "Upconv3d_3a",
+        "Conv3d_3b_3x3",
+        "Conv3d_3c_3x3",
+        "Upconv3d_4a",
+        "Conv3d_4b_3x3",
+        "Conv3d_4c_3x3",
+        "Upconv3d_5a",
+        "Conv3d_5b_3x3",
+        "Upconv3d_6",
+    )
+
+    def __init__(self, in_channels: int, latent_dim: int):
+        super(DecoderConv3D, self).__init__()
+
+        self.end_points = {}
+
+        end_point = "Conv3d_1_1x1"
+        self.end_points[end_point] = Unit3D(
+            in_channels=latent_dim,
+            # output_channels=58,
+            output_channels=28,
+            kernel_shape=[1, 1, 1],
+            padding=0,
+            name=end_point,
+        )
+
+        end_point = "Upconv3d_2a"
+        # block_dim = 58
+        block_dim = 28
+        self.end_points[end_point] = nn.Sequential(
+            nn.ConvTranspose3d(
+                block_dim, block_dim, (4, 3, 3), (2, 2, 2), 1, bias=False
+            ),
+            nn.BatchNorm3d(block_dim),
+            nn.ReLU(inplace=True),
+        )
+        end_point = "Conv3d_2b_3x3"
+        self.end_points[end_point] = Unit3D(
+            in_channels=2 * block_dim,
+            output_channels=block_dim,
+            kernel_shape=[3, 3, 3],
+            padding=0,
+            name=end_point,
+        )
+        end_point = "Conv3d_2c_3x3"
+        self.end_points[end_point] = Unit3D(
+            in_channels=block_dim,
+            # output_channels=32,
+            output_channels=16,
+            kernel_shape=[3, 3, 3],
+            padding=0,
+            name=end_point,
+        )
+
+        end_point = "Upconv3d_3a"
+        # block_dim = 32
+        block_dim = 16
+        self.end_points[end_point] = nn.Sequential(
+            nn.ConvTranspose3d(block_dim, block_dim, 4, 2, 1, bias=False),
+            nn.BatchNorm3d(block_dim),
+            nn.ReLU(inplace=True),
+        )
+        end_point = "Conv3d_3b_3x3"
+        self.end_points[end_point] = Unit3D(
+            in_channels=2 * block_dim,
+            output_channels=block_dim,
+            kernel_shape=[3, 3, 3],
+            padding=0,
+            name=end_point,
+        )
+        end_point = "Conv3d_3c_3x3"
+        self.end_points[end_point] = Unit3D(
+            in_channels=block_dim,
+            # output_channels=8,
+            output_channels=4,
+            kernel_shape=[3, 3, 3],
+            padding=0,
+            name=end_point,
+        )
+
+        end_point = "Upconv3d_4a"
+        # block_dim = 8
+        block_dim = 4
+        self.end_points[end_point] = nn.Sequential(
+            nn.ConvTranspose3d(
+                block_dim, block_dim, (3, 4, 4), (1, 2, 2), 1, bias=False
+            ),
+            nn.BatchNorm3d(block_dim),
+            nn.ReLU(inplace=True),
+        )
+        end_point = "Conv3d_4b_3x3"
+        self.end_points[end_point] = Unit3D(
+            in_channels=2 * block_dim,
+            output_channels=block_dim,
+            kernel_shape=[3, 3, 3],
+            padding=0,
+            name=end_point,
+        )
+        end_point = "Conv3d_4c_3x3"
+        self.end_points[end_point] = Unit3D(
+            in_channels=block_dim,
+            # output_channels=4,
+            output_channels=3,
+            kernel_shape=[3, 3, 3],
+            padding=0,
+            name=end_point,
+        )
+
+        end_point = "Upconv3d_5a"
+        # block_dim = 4
+        block_dim = 3
+        self.end_points[end_point] = nn.Sequential(
+            nn.ConvTranspose3d(
+                block_dim, block_dim, (3, 4, 4), (1, 2, 2), 1, bias=False
+            ),
+            nn.BatchNorm3d(block_dim),
+            nn.ReLU(inplace=True),
+        )
+        end_point = "Conv3d_5b_3x3"
+        self.end_points[end_point] = Unit3D(
+            in_channels=2 * block_dim,
+            output_channels=block_dim,
+            kernel_shape=[3, 3, 3],
+            padding=0,
+            name=end_point,
+        )
+
+        end_point = "Upconv3d_6"
+        self.end_points[end_point] = nn.Sequential(
+            # nn.ConvTranspose3d(4, 2, (4, 6, 6), (2, 4, 4), 1, bias=False),
+            nn.ConvTranspose3d(3, 2, (4, 6, 6), (2, 4, 4), 1, bias=False),
+            nn.BatchNorm3d(2),
+        )
+
+        self.tanh = nn.Tanh()
+        self.build()
+
+    def build(self):
+        for k in self.end_points.keys():
+            self.add_module(k, self.end_points[k])
+
+    def forward(self, x, residual_features) -> torch.Tensor:
+        up_conv_count = 0
+        for end_point in self.decoder.VALID_ENDPOINTS:
+            x = self.decoder._modules[end_point](x)
+            print(end_point, x.shape)
+            if (
+                isinstance(self.decoder._modules[end_point], nn.Sequential)
+                and end_point != "Upconv3d_6"
+            ):
+                up_conv_count += 1
+                x = torch.hstack([x, residual_features[-up_conv_count]])
+        out = self.decoder.tanh(x)
+        return out
+
+
+class SimpleAutoencoderI3D(nn.Module):
+    """Flow autoencoder with an I3D encoder."""
+
+    def __init__(self, in_channels: int, latent_dim: int):
+        super(SimpleAutoencoderI3D, self).__init__()
+        self.encoder = ReducedInceptionI3d(in_channels=in_channels)
+        self.decoder = SimpleDecoderConv3D(in_channels, latent_dim)
+
+    def forward(self, x) -> Tuple[torch.Tensor, torch.Tensor]:
+        latent_features = self.encoder.extract_features(x, avg_out=False)
+        out = self.decoder(latent_features)
+
+        return latent_features, out
+
+    def extract_features(self, x) -> torch.Tensor:
+        latent_features = self.encoder.extract_features(x, avg_out=False)
+        return latent_features
+
+
 class AutoencoderI3D(nn.Module):
     """Flow autoencoder with an I3D encoder."""
 
     def __init__(self, in_channels: int, latent_dim: int):
         super(AutoencoderI3D, self).__init__()
-        self.encoder = InceptionI3d(in_channels=in_channels)
+        self.encoder = ReducedInceptionI3d(in_channels=in_channels)
         self.decoder = DecoderConv3D(in_channels, latent_dim)
-        self.avg_pool = nn.AvgPool3d(kernel_size=[2, 7, 7], stride=(1, 1, 1))
 
     def forward(self, x) -> Tuple[torch.Tensor, torch.Tensor]:
-        latent_features = self.encoder.extract_features(x, avg_out=False)
-        flatten_features = self.avg_pool(latent_features)
-        out = self.decoder(latent_features)
+        # Encode
+        residual_features = []
+        for end_point in self.encoder.VALID_ENDPOINTS:
+            if end_point in self.encoder.end_points:
+                if isinstance(self.encoder._modules[end_point], nn.MaxPool3d):
+                    residual_features.append(x)
+                x = self.encoder._modules[end_point](x)
+            print(end_point, x.shape)
+        latent_features = x
 
-        return flatten_features, out
+        # Decode
+        up_conv_count = 0
+        for end_point in self.decoder.VALID_ENDPOINTS:
+            x = self.decoder._modules[end_point](x)
+            if (
+                isinstance(self.decoder._modules[end_point], nn.Sequential)
+                and end_point != "Upconv3d_6"
+            ):
+                up_conv_count += 1
+                x = torch.hstack([x, residual_features[-up_conv_count]])
+            print(end_point, x.shape)
+        out = self.decoder.tanh(x)
 
-    def extract_features(self, x) -> torch.Tensor:
-        latent_features = self.encoder.extract_features(x, avg_out=True)
-        return latent_features
+        return latent_features, out
+
+    def extract_features(self, x) -> Tuple[torch.Tensor, torch.Tensor]:
+        for end_point in self.encoder.VALID_ENDPOINTS:
+            if end_point in self.encoder.end_points:
+                x = self.encoder._modules[end_point](x)
+        return x
 
 
 def make_flow_encoder(pretrained_path: str) -> nn.Module:
@@ -513,11 +929,17 @@ def make_flow_autoencoder(pretrained_path: str) -> nn.Module:
     :param pretrained_path: if provided load the model stored at this location.
     :return: configured flow autoencoder.
     """
-    model = AutoencoderI3D(in_channels=2, latent_dim=1024)
+    model = AutoencoderI3D(in_channels=2, latent_dim=32)
 
-    # TODO
-    # if pretrained_path:
-    #     pretrained_params = torch.load(pretrained_path)
-    #     model.load_state_dict(pretrained_params)
+    if pretrained_path:
+        pretrained_params = torch.load(pretrained_path)
+        model.load_state_dict(pretrained_params)
 
     return model
+
+
+if __name__ == "__main__":
+    model = AutoencoderI3D(in_channels=2, latent_dim=32)
+
+    x = torch.rand((1, 2, 16, 224, 224))
+    model(x)
