07a7087e9050c5f176741947f81549d51197f059
robincourant <courant.robin@hotmail.fr>
2022-05-04 14:42:18 +0200

    Add ranking loss

---
+++ flow_encoder/src/models/metrics/ranking.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+
+
+class RankingLoss(nn.Module):
+    def __init__(self, margin: float = 2.0):
+        super(RankingLoss, self).__init__()
+        self.pdist = nn.PairwiseDistance(p=2)
+        self.margin = margin
+
+    def forward(self, r0: torch.Tensor, r1: torch.Tensor, y: torch.Tensor):
+        batch_size = r0.shape[0]
+        distance = self.pdist(r0.view(batch_size, -1), r1.view(batch_size, -1))
+        zero = torch.zeros_like(distance)
+        loss = y * distance + (1 - y) * torch.max(zero, self.margin - distance)
+        return loss.mean()
