1b2d01fd6ea7e94fa565149eb71a3db0f5bab8f7
robincourant <courant.robin@hotmail.fr>
2022-05-05 10:39:19 +0200

    Add misc script

---
+++ flow_encoder/src/datamodules/datasets/mask_dataset.py
@@ -0,0 +1,149 @@
+import os
+import os.path as osp
+from typing import Any, Dict, List
+
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+
+from utils.file_utils import load_pth
+
+
+class MaskFlowDataset(Dataset):
+    """Load a temporally and/or spatially masked sample from precomputed flows.
+
+    :param flow_dir: path to the directory with precomputed flows.
+    :param n_frames: number of flow frames in a sample.
+    :param stride: number of flow frames between 2 consecutive samples.
+    :param n_temporal_mask: number of frame to keep (not masked).
+    :param spatial_mask_size: size of the of the frame to keep (not masked).
+    """
+
+    def __init__(
+        self,
+        flow_dir: str,
+        n_frames: int,
+        stride: int,
+        n_temporal_mask: int,
+        spatial_mask_size: int,
+    ):
+        super().__init__()
+
+        self._flow_dir = flow_dir
+        self._clip_dirnames = sorted(os.listdir(flow_dir))
+
+        self._n_frames = n_frames
+        self._stride = stride
+
+        self._n_temporal_mask = n_temporal_mask
+        self._spatial_mask_size = spatial_mask_size
+
+        self._clip_infos = self._get_clip_infos()
+        self._sample_infos = self._get_sample_infos()
+
+    @staticmethod
+    def _split_chunks(array: List[Any], stride: int, chunk_size: int):
+        """successive n-sized chunks from `array`."""
+        for i in range(0, len(array), stride):
+            array[i : i + chunk_size]
+
+    @staticmethod
+    def _load_flows(flow_paths: List[str]) -> torch.Tensor:
+        """Load flows. Output shape: (C, T, W, H)."""
+        flows = torch.stack([load_pth(flow_path) for flow_path in flow_paths])
+        flows = flows.permute([3, 0, 1, 2])
+
+        return flows
+
+    def _get_clip_infos(self) -> List[Dict[str, Any]]:
+        """Get precomputed flow paths for all samples.
+
+        :return: a list of clips with:
+            - `clip_name`: name of the clip.
+            - `flow_paths`: paths of the flow precomputed flows.
+        """
+        clip_infos = []
+        for clip_dirname in sorted(self._clip_dirnames):
+            flow_clip_dir = osp.join(self._flow_dir, clip_dirname)
+
+            flow_paths = []
+            flow_names = os.listdir(flow_clip_dir)
+            for flow_filename in sorted(flow_names):
+                flow_paths.append(osp.join(flow_clip_dir, flow_filename))
+
+            clip_infos.append(
+                {
+                    "clip_name": clip_dirname,
+                    "flow_paths": flow_paths,
+                }
+            )
+
+        return clip_infos
+
+    def _get_sample_infos(self) -> List[Dict[str, Any]]:
+        """
+        Each sample is composed of `n_frames` consecutive flows.
+
+        :return: a list of samples with:
+            - `clipname`: sample clip name.
+            - `flow_paths`: paths of the `n_frames` flows.
+        """
+        # Start by splitting each clip into chunks of `n_frames`
+        sample_splits, sample_clipnames = np.empty((0,)), np.empty((0,))
+        for clip_info in self._clip_infos:
+            clip_name = clip_info["clip_name"]
+            flow_paths = clip_info["flow_paths"]
+
+            flow_gen = self._split_chunks(
+                flow_paths, self._stride, self._n_frames
+            )
+            for chunk_index, flow_chunk in enumerate(flow_gen):
+                if len(flow_chunk) != self._n_frames:
+                    break
+                frame_start = self._stride * chunk_index
+                frame_end = frame_start + self._n_frames - 1
+                chunk_infos = np.array(
+                    {
+                        "clip_name": clip_name
+                        + f"/{frame_start:04}-{frame_end:04}",
+                        "flow_chunk_paths": flow_chunk,
+                    }
+                ).reshape(1)
+                sample_splits = np.hstack([sample_splits, chunk_infos])
+                sample_clipnames = np.hstack([sample_clipnames, clip_name])
+
+        # Then generate the triplet samples (anchor. positive, negative)
+        sample_infos = []
+        for sample in sample_splits:
+            sample_infos.append(
+                {
+                    "clipname": sample["clip_name"],
+                    "flow_paths": sample["flow_chunk_paths"],
+                }
+            )
+
+        return sample_infos
+
+    def __len__(self) -> int:
+        return len(self._sample_infos)
+
+    def __getitem__(self, index: int) -> Dict[str, Any]:
+        """Return the `index`-th sample.
+
+        :return: a sample with:
+            - `clipname`: positive and anchor sample clip name.
+            - `flows`: flows of the anchor.
+        """
+        sample_info = self._sample_infos[index]
+
+        flow_paths = sample_info["flow_paths"]
+        flows = self._load_flows(flow_paths)
+        masked_flows = torch.zeros_like(flows)
+        # masked_flows[:, self._n_temporal_mask :] = flows[
+        #     :, self._n_temporal_mask :
+        # ]
+        masked_flows[:, :, 102:122, 102:122] = flows[:, :, 102:122, 102:122]
+
+        sample_data = {"clipname": sample_info["clipname"], "flows": flows}
+
+        return sample_data
---
+++ flow_encoder/src/prune_model.py
@@ -0,0 +1,92 @@
+import os
+import os.path as osp
+
+import hydra
+import matplotlib.pyplot as plt
+from omegaconf import DictConfig
+import torch
+import torch.nn as nn
+import torch.nn.utils.prune as prune
+from flow_encoder.src.models.flow_autoencoder import I3DAutoencoderModel
+from utils.file_utils import load_pth, create_dir
+from utils.flow_utils import FlowUtils
+
+
+@hydra.main(config_path="../configs/", config_name="config.yaml")
+def main(config: DictConfig):
+    flow_utils = FlowUtils()
+
+    # Load and save test chunk
+    clip_name = os.listdir(config.datamodule.unity_dir)[0]
+    clip_dir = osp.join(config.datamodule.unity_dir, clip_name)
+    flow_filenames = sorted(os.listdir(clip_dir))[:16]
+    flow_chunk = torch.stack(
+        [
+            load_pth(osp.join(clip_dir, filename))
+            for filename in sorted(flow_filenames)
+        ]
+    ).permute([3, 0, 1, 2])
+    pruning_res_dir = osp.join(config.viz_dir, "pruning_results")
+    ref_res_dir = osp.join(pruning_res_dir, "ref")
+    create_dir(ref_res_dir)
+    for k, flow in enumerate(flow_chunk.permute([1, 2, 3, 0])):
+        frame = flow_utils.flow_to_frame(flow.numpy())
+        plt.imsave(osp.join(ref_res_dir, str(k).zfill(2) + ".png"), frame)
+
+    model_params = {
+        "histogram": config.model.histogram,
+        "pretrained_path": None,
+        "optimizer": config.model.optimizer,
+        "learning_rate": config.model.learning_rate,
+        "weight_decay": config.model.weight_decay,
+        "momentum": config.model.momentum,
+        "batch_size": config.compnode.batch_size,
+        "check_dir": config.viz_dir,
+        "flow_type": config.datamodule.flow_type,
+        "checkpoint_path": config.model.checkpoint_path,
+    }
+
+    # for pruning_rate in [0.0, 0.2, 0.4, 0.6, 0.8, 0.99]:
+    for pruning_rate in [0.8]:
+        # Initialize model
+        model = I3DAutoencoderModel.load_from_checkpoint(**model_params)
+        encoder = model.model.encoder
+        parameters_to_prune = []
+        # Prune model
+        for mod in encoder.modules():
+            if isinstance(mod, nn.Conv3d):
+                parameters_to_prune.append((mod, "weight"))
+        prune.global_unstructured(
+            parameters_to_prune,
+            pruning_method=prune.L1Unstructured,
+            amount=pruning_rate,
+        )
+        n_zeros, n_elements = 0, 0
+
+        for mod_index, (name, mod) in enumerate(
+            model.model.encoder.named_modules()
+        ):
+            if isinstance(mod, nn.Conv3d):
+                print(
+                    name,
+                    float(torch.sum(mod.weight == 0) / mod.weight.nelement()),
+                    mod,
+                )
+                n_zeros += torch.sum(mod.weight == 0)
+                n_elements += mod.weight.nelement()
+
+        sparsity = n_zeros / n_elements
+        print(f"Sparsity: {sparsity:.2f}")
+        # Infer model and save results
+        _, reconstructed_flow = model.model(flow_chunk[None])
+        current_res_dir = osp.join(pruning_res_dir, str(pruning_rate).zfill(3))
+        create_dir(current_res_dir)
+        for k, flow in enumerate(reconstructed_flow[0].permute([1, 2, 3, 0])):
+            frame = flow_utils.flow_to_frame(flow.detach().numpy())
+            plt.imsave(
+                osp.join(current_res_dir, str(k).zfill(2) + ".png"), frame
+            )
+
+
+if __name__ == "__main__":
+    main()
