0aabd259abc5f67f44dc01f586033ce56c24c6bd
robincourant <courant.robin@hotmail.fr>
2022-08-23 14:08:29 +0200

    Add 2x nested folder

--- flow_encoder/src/datamodules/preprocessing/pkl_to_pth.py
+++ flow_encoder/src/datamodules/preprocessing/pkl_to_pth.py
@@ -17,7 +17,7 @@ def parse_arguments() -> Tuple[str, str]:
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        "pk_dir",
+        "flow_dir",
         type=str,
         help="Path to the directory containing the pickle files",
     )
@@ -28,21 +28,42 @@ def parse_arguments() -> Tuple[str, str]:
     )
     args = parser.parse_args()
 
-    return args.pk_dir, args.pth_dir
+    return args.flow_dir, args.pth_dir
 
 
+# 2x Nested clips /data/dir/video_id/shot_index
 if __name__ == "__main__":
-    pk_dir, pth_dir = parse_arguments()
+    flow_dir, pth_dir = parse_arguments()
 
-    for pk_filename in tqdm(os.listdir(pk_dir)):
-        # Load whole video flows
-        pk_path = osp.join(pk_dir, pk_filename)
-        flows = load_pickle(pk_path)
+    for shot_filename in tqdm(sorted(os.listdir(flow_dir))):
+        pk_dir = osp.join(flow_dir, shot_filename)
+        for pk_filename in sorted(os.listdir(pk_dir)):
+            pk_path = osp.join(pk_dir, pk_filename)
+            save_dir = osp.join(pth_dir, shot_filename, pk_filename[:-3])
+            if osp.exists(save_dir):
+                continue
+            create_dir(save_dir)
+            # Load whole video flows
+            flows = load_pickle(pk_path)
+            # Save flows as pytorch tensors
+            for k in range(len(flows)):
+                flow_filename = osp.join(save_dir, str(k).zfill(4) + ".pth")
+                flow_tensor = torch.from_numpy(flows[k])
+                save_pth(flow_tensor, flow_filename)
 
-        # Save flows as pytorch tensors
-        flow_dir = osp.join(pth_dir, pk_filename[:-3])
-        create_dir(flow_dir)
-        for k in range(len(flows)):
-            flow_filename = osp.join(flow_dir, str(k).zfill(4) + ".pth")
-            flow_tensor = torch.from_numpy(flows[k])
-            save_pth(flow_tensor, flow_filename)
+# 1x Nested clips /data/dir/video_id
+# if __name__ == "__main__":
+#     pk_dir, pth_dir = parse_arguments()
+
+#     for pk_filename in tqdm(os.listdir(pk_dir)):
+#         # Load whole video flows
+#         pk_path = osp.join(pk_dir, pk_filename)
+#         flows = load_pickle(pk_path)
+
+#         # Save flows as pytorch tensors
+#         flow_dir = osp.join(pth_dir, pk_filename[:-3])
+#         create_dir(flow_dir)
+#         for k in range(len(flows)):
+#             flow_filename = osp.join(flow_dir, str(k).zfill(4) + ".pth")
+#             flow_tensor = torch.from_numpy(flows[k])
+#             save_pth(flow_tensor, flow_filename)
--- raw_features/scripts/compute_flows.py
+++ raw_features/scripts/compute_flows.py
@@ -47,29 +47,71 @@ def parse_arguments():
     return args
 
 
+# 2x Nested clips /data/dir/video_id/shot_index
 if __name__ == "__main__":
     args = parse_arguments()
-    video_dir = args.video_dir
+    frame_dir = args.video_dir
     save_dir = args.save_dir
     model_name = args.model_name
     video_ids_filename = args.video_ids
 
     create_dir(save_dir)
     if video_ids_filename is None:
-        video_ids = os.listdir(video_dir)
+        video_ids = os.listdir(frame_dir)
     else:
         video_ids = load_pickle(video_ids_filename)
-
+    orig_dir = "/media/hdd_robin/MovieShot/flows"
     flow_estimator = FlowEstimator(model_name=model_name, batch_size=1)
-    for video_id in tqdm(video_ids):
-        video_path = osp.join(video_dir, video_id)
-        flow_path = osp.join(save_dir, video_id + ".pk")
-        # if osp.exists(flow_path):
-        #     continue
-        # Load frames and convert them from BRG to RGB
-        frames = [
-            cv2.cvtColor(cv2.resize(f, (224, 224)), cv2.COLOR_BGR2RGB)
-            for f in load_frames_fromdir(video_path)
-        ]
-        flows = flow_estimator.estimate_flow(frames)
-        save_pickle(flows, flow_path)
+
+    for video_id in tqdm(sorted(video_ids)[:7200][::-1]):
+        video_dir = osp.join(frame_dir, video_id)
+        flow_dir = osp.join(save_dir, video_id)
+        orig_flow_dir = osp.join(orig_dir, video_id)
+        create_dir(flow_dir)
+        for shot_index in sorted(os.listdir(video_dir)):
+            video_path = osp.join(video_dir, shot_index)
+            flow_path = osp.join(flow_dir, shot_index + ".pk")
+            orig_flow_path = osp.join(orig_flow_dir, shot_index + ".pk")
+            if osp.exists(orig_flow_path) or osp.exists(flow_path):
+                continue
+            # Load frames and convert them from BRG to RGB
+            try:
+                frames = [
+                    cv2.cvtColor(cv2.resize(f, (224, 224)), cv2.COLOR_BGR2RGB)
+                    for f in load_frames_fromdir(video_path)
+                ]
+                flows = flow_estimator.estimate_flow(frames)
+                save_pickle(flows, flow_path)
+            except:
+                import ipdb
+
+                ipdb.set_trace()
+
+
+# 1x Nested clips /data/dir/video_id
+# if __name__ == "__main__":
+#     args = parse_arguments()
+#     video_dir = args.video_dir
+#     save_dir = args.save_dir
+#     model_name = args.model_name
+#     video_ids_filename = args.video_ids
+
+#     create_dir(save_dir)
+#     if video_ids_filename is None:
+#         video_ids = os.listdir(video_dir)
+#     else:
+#         video_ids = load_pickle(video_ids_filename)
+
+#     flow_estimator = FlowEstimator(model_name=model_name, batch_size=1)
+#     for video_id in tqdm(video_ids):
+#         video_path = osp.join(video_dir, video_id)
+#         flow_path = osp.join(save_dir, video_id + ".pk")
+#         # if osp.exists(flow_path):
+#         #     continue
+#         # Load frames and convert them from BRG to RGB
+#         frames = [
+#             cv2.cvtColor(cv2.resize(f, (224, 224)), cv2.COLOR_BGR2RGB)
+#             for f in load_frames_fromdir(video_path)
+#         ]
+#         flows = flow_estimator.estimate_flow(frames)
+#         save_pickle(flows, flow_path)
